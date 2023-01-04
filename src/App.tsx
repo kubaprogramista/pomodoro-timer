4f3c19957cd095f28cb0418628229215131693ac
jacod <jakubcoding@gmail.com>
2023-01-04 14:40:53 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import Heading from "./components/Heading";
+import Form from "./components/Form";
 
 function App() {
-  return <Heading title={"Hello?"} />;
+  let numberOne = document.querySelector(".number1");
+  console.log(numberOne);
+
+  const add = (v1: number, v2: number) => v1 + v2;
+  return (
+    <>
+      <Heading title={"Put 2 numbers"} />
+      <Form />
+    </>
+  );
 }
 
 export default App;
