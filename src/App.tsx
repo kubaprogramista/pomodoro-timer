1d67d6faee005485a0ef806c07b91fd239119db0
jacod <jakubcoding@gmail.com>
2023-01-04 17:55:54 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Heading from "./components/Heading";
 import Timer from "./components/Timer";
+import Settings from "./components/Settings";
 
 function App() {
-  let inputMinutes: number = 30;
+  let inputMinutes: number = 25;
   let inputSeconds: number = 0;
   return (
     <>
-      <Heading title={""} />
+      <Heading title={"Pomodoro Timer"} />
+      <Settings />
       <Timer inputMinutes={inputMinutes} inputSeconds={inputSeconds} />
     </>
   );
