6a18f6fbf02fedfbf289c092bb08d2a31908a4b6
jacod <jakubcoding@gmail.com>
2023-01-04 21:05:30 +0100

    Update Timer.tsx

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 
-type inputs = { inputMinutes: number; inputSeconds: number };
+type dataTypes = { inputMinutes: number; inputSeconds: number; show: boolean };
 
-const Timer = ({ inputMinutes, inputSeconds }: inputs) => {
+const Timer = ({ inputMinutes, inputSeconds, show }: dataTypes) => {
   const date: Date = new Date(0);
   let startingMinutes: number = date.getMinutes() + inputMinutes;
   let startingSeconds: number = date.getSeconds() + inputSeconds;
@@ -11,28 +11,26 @@ const Timer = ({ inputMinutes, inputSeconds }: inputs) => {
   const [seconds, setSeconds] = useState(startingSeconds);
 
   useEffect(() => {
-    if (seconds === 0 && minutes === 0) return;
+    if (show) {
+      if (seconds === 0 && minutes === 0) return;
 
-    const countdown = setTimeout(() => {
-      setSeconds(seconds - 1);
-      if (seconds === 0) {
-        setSeconds(seconds + 59);
-        setMinutes(minutes - 1);
-      }
-    }, 1000);
+      const countdown = setTimeout(() => {
+        setSeconds(seconds - 1);
+        if (seconds === 0) {
+          setSeconds(seconds + 59);
+          setMinutes(minutes - 1);
+        }
+      }, 1000);
+    }
   }, [seconds]);
 
-  let result: string = `${minutes}:${seconds}`;
+  let result: string;
 
-  // if (`${seconds}`.length === 1) {
-  //   result = `${minutes}:0${seconds}`;
-  // } else if (`${minutes}`.length === 1) {
-  //   result = `0${minutes}:${seconds}`;
-  // } else if (`${seconds}`.length === 1 && `${minutes}`.length === 1) {
-  //   result = `0${minutes}:0${seconds}`;
-  // }
-
-  result = `${("0" + minutes).slice(-2)}:${("0" + seconds).slice(-2)}`;
+  if (show) {
+    result = `${("0" + minutes).slice(-2)}:${("0" + seconds).slice(-2)}`;
+  } else {
+    result = "00:00";
+  }
 
   return (
     <>
