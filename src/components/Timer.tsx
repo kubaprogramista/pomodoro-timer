d6a7e9078da86091fef6b6b74cf8f46327611c0b
jacod <jakubcoding@gmail.com>
2023-01-05 04:48:25 +0100

    Update Timer.tsx

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,33 +1,84 @@
 import { useEffect, useState } from "react";
 
-type dataTypes = { inputMinutes: number; inputSeconds: number; show: boolean };
+type dataTypes = {
+  inputMinutes: number;
+  inputSeconds: number;
+  inputBreakMinutes: number;
+  inputBreakSeconds: number;
+  show: boolean;
+};
 
-const Timer = ({ inputMinutes, inputSeconds, show }: dataTypes) => {
+const Timer = ({
+  inputMinutes,
+  inputSeconds,
+  inputBreakMinutes,
+  inputBreakSeconds,
+  show,
+}: dataTypes) => {
   const date: Date = new Date(0);
   let startingMinutes: number = date.getMinutes() + inputMinutes;
   let startingSeconds: number = date.getSeconds() + inputSeconds;
 
+  let breakStartingMinutes: number = date.getMinutes() + inputBreakMinutes;
+  let breakStartingSeconds: number = date.getSeconds() + inputBreakSeconds;
+
   const [minutes, setMinutes] = useState(startingMinutes);
   const [seconds, setSeconds] = useState(startingSeconds);
+  const [breakMinutes, setBreakMinutes] = useState(breakStartingMinutes);
+  const [breakSeconds, setBreakSeconds] = useState(breakStartingSeconds);
+
+  const [isBreak, setIsBreak] = useState(false);
 
   useEffect(() => {
-    if (seconds === 0 && minutes === 0) return;
-
-    setTimeout(() => {
-      setSeconds(seconds - 1);
-      if (seconds === 0) {
-        setSeconds(seconds + 59);
-        setMinutes(minutes - 1);
-      }
-    }, 1000);
-  }, [seconds]);
+    if (!isBreak) {
+      setTimeout(() => {
+        setIsBreak(false);
+        setSeconds(seconds - 1);
+        if (seconds === 0) {
+          setSeconds(seconds + 59);
+          setMinutes(minutes - 1);
+        }
+        if (seconds === 0 && minutes === 0) {
+          setIsBreak(true);
+        }
+        if (breakSeconds === 0 && breakMinutes === 0) {
+          setIsBreak(true);
+        }
+      }, 1000);
+    } else if (isBreak) {
+      setTimeout(() => {
+        setIsBreak(true);
+        setBreakSeconds(breakSeconds - 1);
+        if (breakSeconds === 0) {
+          setBreakSeconds(breakSeconds + 59);
+          setBreakMinutes(breakMinutes - 1);
+        }
+        if (seconds === 0 && minutes === 0) {
+          setIsBreak(false);
+        }
+        if (breakSeconds === 0 && breakMinutes === 0) {
+          setIsBreak(false);
+        }
+      }, 1000);
+    }
+  }, [seconds, breakSeconds]);
 
   let result: string;
-
-  if (show) {
-    result = `${("0" + minutes).slice(-2)}:${("0" + seconds).slice(-2)}`;
-  } else {
-    result = "00:00";
+  result = "00:00";
+  if (!isBreak) {
+    if (show) {
+      result = `${("0" + minutes).slice(-2)}:${("0" + seconds).slice(-2)}`;
+    } else {
+      result = "00:00";
+    }
+  } else if (isBreak) {
+    if (show) {
+      result = `${("0" + breakMinutes).slice(-2)}:${("0" + breakSeconds).slice(
+        -2
+      )}`;
+    } else {
+      result = "00:00";
+    }
   }
 
   return (
