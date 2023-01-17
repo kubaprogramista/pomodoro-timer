5d249907ef89dd547ac35eec9d97a509eae4ff48
jacod <jakubcoding@gmail.com>
2023-01-17 16:26:36 +0100

    Update AudioHandler.tsx

--- src/components/AudioHandler.tsx
+++ src/components/AudioHandler.tsx
@@ -1,9 +1,6 @@
 const AudioHandler = () => {
   return (
-    <>
-      <audio controls src="./src/assets/alarm.mp3" autoPlay muted loop></audio>
-      <img src="./src/assets/tomato.png" alt="" />
-    </>
+    <audio controls src="../src/assets/alarm.mp3" autoPlay muted loop></audio>
   );
 };
 
